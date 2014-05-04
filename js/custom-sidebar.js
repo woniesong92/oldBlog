96ebe8ef0b2d4ead54d26db4ccccaed86e57fea6
maxsong1002 <maxsong1002@hotmail.com>
2014-05-04 17:42:55 +0900

    d

--- js/custom-sidebar.js
+++ js/custom-sidebar.js
@@ -1,17 +1,16 @@
 // Sidebar animations
 $('#sidebar').hover(function() {
-  $(this).animate({ width: '20%' }, 800);
-  $('.extra-links').fadeIn(800);
-  $('.name-small').fadeOut(400, function() {
-    $(this).text('Howon Song').css('font-size', '1.5em').fadeIn(400);
+  //$(this).animate({ width: '20%' }, 800);
+  $('.extra-links').fadeIn(400);
+  $('.name-small').fadeOut(200, function() {
+    $(this).text('Howon Song').css('font-size', '1.5em').fadeIn(200);
   });
-  //$('.name-big').fadeIn();
 
 }, function() {
-  $(this).animate({ width: '16%' }, 800);
-  $('.extra-links').fadeOut(800);
-  $('.name-small').fadeOut(400, function() {
-    $(this).text('HWS').css('font-size', '1.0em').fadeIn(400);
+  //$(this).animate({ width: '16%' }, 800);
+  $('.extra-links').fadeOut(400);
+  $('.name-small').fadeOut(200, function() {
+    $(this).text('HWS').css('font-size', '1.0em').fadeIn(200);
   });
 });
 
