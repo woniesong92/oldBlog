2df72e88ece612f95229121e925d12f23ecbc6e9
Howon Song <hs459@cornell.edu>
2014-08-23 11:25:26 -0400

    content changed

--- js/custom-sidebar.js
+++ js/custom-sidebar.js
@@ -1,17 +1,20 @@
 // Sidebar animations
-$('#sidebar').hover(function() {
-  //$(this).animate({ width: '20%' }, 800);
-  $('.extra-links').fadeIn(400);
-  $('.name-small').fadeOut(200, function() {
-    $(this).text('Howon Song').css('font-size', '1.5em').fadeIn(200);
+
+$(function() {
+  $('#sidebar').hover(function() {
+    //$(this).animate({ width: '20%' }, 800);
+    $('.extra-links').fadeIn(400);
+    $('.name-small').fadeOut(200, function() {
+      $(this).text('Howon Song').css('font-size', '1.5em').fadeIn(200);
+    });
+
+  }, function() {
+    //$(this).animate({ width: '16%' }, 800);
+    $('.extra-links').fadeOut(400);
+    $('.name-small').fadeOut(200, function() {
+      $(this).text('HWS').css('font-size', '1.0em').fadeIn(200);
+    });
   });
 
-}, function() {
-  //$(this).animate({ width: '16%' }, 800);
-  $('.extra-links').fadeOut(400);
-  $('.name-small').fadeOut(200, function() {
-    $(this).text('HWS').css('font-size', '1.0em').fadeIn(200);
-  });
-});
-
-$('#sidebar .my-name').css('margin-top', $(this).height()/3);
+  $('#sidebar .my-name').css('margin-top', $(this).height()/3);
+})
