eda1f8170e94cc177250f98be05f5aa36aa9e960
Howon Song <hs459@cornell.edu>
2014-08-24 09:39:27 -0400

    sidebar for mobile

--- js/app.js
+++ js/app.js
@@ -3,16 +3,41 @@ $(function() {
 
   app.showSidebar = function(delay) {
     var delay = delay || 400;
-    var $sidebar = $('#sidebar');
+    var $sidebar = $('.sidebar');
     $sidebar.width(0);
-    $sidebar.show().animate({width: '250px'}, delay);
+    var htmlWidth = $('html').width();
+    var newWidth = (htmlWidth > 768) ? "250px" : "150px";
+    $sidebar.show().animate({width: newWidth}, delay);
     sessionStorage.setItem('sidebar', true);
   },
 
   app.hideSidebar = function(delay) {
     var delay = delay || 400;
-    var $sidebar = $('#sidebar');
+    var $sidebar = $('.sidebar');
     $sidebar.animate({ width: 0 }, delay);
     sessionStorage.setItem('sidebar', false);
   }
+
+  var isSidebar = sessionStorage.getItem('sidebar');
+  if (isSidebar == "true") $('.sidebar').show();
+  else $('.sidebar').hide();
+
+  /* default.html */
+  $('#about-link').click(function() {
+    $.scrollTo('#about', 400);
+  });
+
+  $('#projects-link').click(function() {
+    $.scrollTo('#projects', 400);
+  });
+
+  $('#menu-show-button').click(function() {
+    app.showSidebar();
+  });
+
+  /* sidebar.html */
+  $('.menu-hide-button').click(function() {
+    app.hideSidebar();
+  });
+
 });
